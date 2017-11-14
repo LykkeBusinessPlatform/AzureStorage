3f9d9c4d3891e59862dabce00b231132c22b6496
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-11-15 00:20:15 +0300

    - Propagation of the user types metadata to the collections and nullable of these types was implemented

--- src/Lykke.AzureStorage/Tables/Entity/Metamodel/EntityMetamodelImpl.cs
+++ src/Lykke.AzureStorage/Tables/Entity/Metamodel/EntityMetamodelImpl.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Lykke.AzureStorage.Tables.Entity.Metamodel.Providers;
 using Lykke.AzureStorage.Tables.Entity.Serializers;
@@ -24,7 +26,10 @@ namespace Lykke.AzureStorage.Tables.Entity.Metamodel
                 // Serializer defined on the property, overrides that one, defined on the type, which property has
 
                 return _provider.TryGetPropertySerializer(property) ??
-                       _provider.TryGetTypeSerializer(property.PropertyType);
+                       _provider.TryGetTypeSerializer(property.PropertyType) ??
+                       TryGetCollectionSerializer(property.PropertyType) ??
+                       TryGetNullableSerializer(property.PropertyType);
+
             }
             catch (Exception ex)
             {
@@ -42,6 +47,70 @@ namespace Lykke.AzureStorage.Tables.Entity.Metamodel
             {
                 throw new InvalidOperationException($"Failed to get value type merging strategy factory for entity type {type}", ex);
             }
+        }
+
+        private IStorageValueSerializer TryGetCollectionSerializer(Type collectionType)
+        {
+            var enumerbleGenericArguments = GetGenericArgumentsOfAssignableType(collectionType, typeof(IEnumerable<>));
+            if (enumerbleGenericArguments != null)
+            {
+                var elementType = enumerbleGenericArguments.First();
+
+                return _provider.TryGetTypeSerializer(elementType);
+            }
+
+            if (collectionType.IsArray)
+            {
+                var elementType = collectionType.GetElementType();
+
+                return _provider.TryGetTypeSerializer(elementType);
+            }
+
+            return null;
+        }
+
+        private IStorageValueSerializer TryGetNullableSerializer(Type nullableType)
+        {
+            var nullableGenericArguments = GetGenericArgumentsOfAssignableType(nullableType, typeof(Nullable<>));
+            if (nullableGenericArguments != null)
+            {
+                var elementType = nullableGenericArguments.First();
+
+                return _provider.TryGetTypeSerializer(elementType);
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Returns generic arguments of type <paramref name="genericType"/>, 
+        /// if <paramref name="givenType"/> is assignable to the <paramref name="genericType"/>,
+        /// or null
+        /// </summary>
+        private static Type[] GetGenericArgumentsOfAssignableType(Type givenType, Type genericType)
+        {
+            if (givenType.IsGenericType && givenType.GetGenericTypeDefinition() == genericType)
+            {
+                return givenType.GenericTypeArguments;
+            }
+
+            var interfaceTypes = givenType.GetInterfaces();
+
+            foreach (var it in interfaceTypes)
+            {
+                if (it.IsGenericType && it.GetGenericTypeDefinition() == genericType)
+                {
+                    return it.GetGenericArguments();
+                }
+            }
+
+            var baseType = givenType.BaseType;
+            if (baseType == null)
+            {
+                return null;
+            }
+
+            return GetGenericArgumentsOfAssignableType(baseType, genericType);
         }
     }
 }
