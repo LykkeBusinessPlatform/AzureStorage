826012abfd97d45c46e9c22f5e2c766ca855db59
Sergey Nesterov <sergey.nesterov@lykke.com>
2018-07-12 12:17:26 +0300

    Added InvalidContinuationTokenException

---
+++ src/Lykke.AzureStorage/Tables/Exceptions/InvalidContinuationTokenException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Lykke.AzureStorage.Tables.Exceptions
+{
+    /// <summary>
+    /// Exception for a case when invalid continuation token is provided.
+    /// </summary>
+    public class InvalidContinuationTokenException : Exception
+    {
+        public InvalidContinuationTokenException()
+        {
+        }
+
+        public InvalidContinuationTokenException(string message) : base(message)
+        {
+        }
+
+        public InvalidContinuationTokenException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidContinuationTokenException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
